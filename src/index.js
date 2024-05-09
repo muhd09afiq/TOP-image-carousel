bc78b548be5339c0974ea8d3672c140caf33e173
blazion09 <muhd09afiq@gmail.com>
2024-05-09 16:20:05 +0800

    chore

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import "./css/reset.css";
 import "./css/style.css";
+
+import img1 from "./img/1.jpg";
+
+const photo1 = new Image();
+photo1.src = img1;
+
+const frame = document.querySelector(".img-frame");
+
+frame.appendChild(photo1);
