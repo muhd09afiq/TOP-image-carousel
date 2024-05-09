4adefddf22c8b96c13884f213b6ecf1db953f1a2
blazion09 <muhd09afiq@gmail.com>
2024-05-09 23:16:39 +0800

    chore

--- src/index.js
+++ src/index.js
@@ -2,10 +2,40 @@ import "./css/reset.css";
 import "./css/style.css";
 
 import img1 from "./img/1.jpg";
-
-const photo1 = new Image();
-photo1.src = img1;
+import img2 from "./img/2.jpg";
+import img3 from "./img/3.jpg";
+import img4 from "./img/4.jpg";
+import img5 from "./img/5.jpg";
 
 const frame = document.querySelector(".img-frame");
 
-frame.appendChild(photo1);
+const imgArray = [img1, img2, img3, img4, img5];
+
+imgArray.forEach((img) => {
+  const photo = new Image();
+  photo.src = img;
+  frame.appendChild(photo);
+  photo.classList.add("hidden");
+  if (img == imgArray[2]) {
+    photo.classList.remove("hidden");
+    photo.classList.add("visible");
+  }
+});
+
+const rightBtn = document.querySelector(".right-btn");
+rightBtn.addEventListener("click", () => {
+  const imgGallery = document.querySelectorAll("img");
+  imgGallery.forEach((img) => {
+    if (img.classList.contains("visible") == true) {
+      img.classList.remove("visible");
+      img.classList.add("hidden");
+
+      //show next img
+      const nextImg = imgGallery[img];
+      console.log(typeof nextImg);
+      console.log(nextImg);
+      //   nextImg.classList.add("visible");
+      //   nextImg.classList.remove("hidden");
+    }
+  });
+});
