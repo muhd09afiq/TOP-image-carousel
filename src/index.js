091dd79ba0af133fd296fbae8d2ae059face88f3
blazion09 <muhd09afiq@gmail.com>
2024-05-10 10:40:15 +0800

    complete carousel

--- src/index.js
+++ src/index.js
@@ -23,12 +23,16 @@ imgArray.forEach((img) => {
   if (img == imgArray[0]) {
     photo.classList.add("first-img");
   }
+  if (img == imgArray[4]) {
+    photo.classList.add("last-img");
+  }
 });
 
 const rightBtn = document.querySelector(".right-btn");
 rightBtn.addEventListener("click", function () {
   const imgGallery = document.querySelectorAll("img");
   imgGallery.forEach((img) => {
+    //remove current image visibility
     if (img.classList.contains("visible") == true) {
       img.classList.remove("visible");
       img.classList.add("hidden");
@@ -36,18 +40,50 @@ rightBtn.addEventListener("click", function () {
     }
   });
   if (document.querySelector(".previous") != null) {
+    //select and show next image
     if (document.querySelector(".previous").nextSibling != null) {
       const nextImg = document.querySelector(".previous").nextSibling;
+      document.querySelector(".previous").classList.remove("previous");
       nextImg.classList.add("visible");
       nextImg.classList.remove("hidden");
       nextImg.classList.add("previous");
-      document.querySelector(".previous").classList.remove("previous");
     } else {
+      //img will rotate back to first img when reaching last image
       document.querySelector(".previous").classList.remove("previous");
       const firstImg = document.querySelector(".first-img");
       firstImg.classList.add("visible");
       firstImg.classList.remove("hidden");
       firstImg.classList.add("previous");
+    }
+  }
+});
+
+const leftBtn = document.querySelector(".left-btn");
+leftBtn.addEventListener("click", function () {
+  const imgGallery = document.querySelectorAll("img");
+  imgGallery.forEach((img) => {
+    //remove current image visibility
+    if (img.classList.contains("visible") == true) {
+      img.classList.remove("visible");
+      img.classList.add("hidden");
+      img.classList.add("previous");
+    }
+  });
+  if (document.querySelector(".previous") != null) {
+    //select and show next image
+    if (document.querySelector(".previous").previousSibling != null) {
+      const nextImg = document.querySelector(".previous").previousSibling;
+      document.querySelector(".previous").classList.remove("previous");
+      nextImg.classList.add("visible");
+      nextImg.classList.remove("hidden");
+      nextImg.classList.add("previous");
+    } else {
+      //img will rotate back to last img when reaching first image
+      document.querySelector(".previous").classList.remove("previous");
+      const lastImg = document.querySelector(".last-img");
+      lastImg.classList.add("visible");
+      lastImg.classList.remove("hidden");
+      lastImg.classList.add("previous");
     }
   }
 });
