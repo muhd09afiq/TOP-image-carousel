8f1e4af0140d8d21827712adb7679ca43cf86536
blazion09 <muhd09afiq@gmail.com>
2024-05-10 11:04:53 +0800

    code refactor complete

--- src/index.js
+++ src/index.js
@@ -11,79 +11,42 @@ const frame = document.querySelector(".img-frame");
 
 const imgArray = [img1, img2, img3, img4, img5];
 
-imgArray.forEach((img) => {
+imgArray.forEach((img, index) => {
   const photo = new Image();
   photo.src = img;
   frame.appendChild(photo);
   photo.classList.add("hidden");
-  if (img == imgArray[2]) {
+  photo.alt = `Photo ${index + 1}`;
+  //show the first image on load
+  if (index === 0) {
+    photo.classList.add("first-img");
     photo.classList.remove("hidden");
     photo.classList.add("visible");
   }
-  if (img == imgArray[0]) {
-    photo.classList.add("first-img");
-  }
-  if (img == imgArray[4]) {
-    photo.classList.add("last-img");
-  }
+  if (index === imgArray.length - 1) photo.classList.add("last-img");
 });
 
 const rightBtn = document.querySelector(".right-btn");
-rightBtn.addEventListener("click", function () {
-  const imgGallery = document.querySelectorAll("img");
-  imgGallery.forEach((img) => {
-    //remove current image visibility
-    if (img.classList.contains("visible") == true) {
-      img.classList.remove("visible");
-      img.classList.add("hidden");
-      img.classList.add("previous");
-    }
-  });
-  if (document.querySelector(".previous") != null) {
-    //select and show next image
-    if (document.querySelector(".previous").nextSibling != null) {
-      const nextImg = document.querySelector(".previous").nextSibling;
-      document.querySelector(".previous").classList.remove("previous");
-      nextImg.classList.add("visible");
-      nextImg.classList.remove("hidden");
-      nextImg.classList.add("previous");
-    } else {
-      //img will rotate back to first img when reaching last image
-      document.querySelector(".previous").classList.remove("previous");
-      const firstImg = document.querySelector(".first-img");
-      firstImg.classList.add("visible");
-      firstImg.classList.remove("hidden");
-      firstImg.classList.add("previous");
-    }
-  }
-});
-
 const leftBtn = document.querySelector(".left-btn");
-leftBtn.addEventListener("click", function () {
-  const imgGallery = document.querySelectorAll("img");
-  imgGallery.forEach((img) => {
-    //remove current image visibility
-    if (img.classList.contains("visible") == true) {
-      img.classList.remove("visible");
-      img.classList.add("hidden");
-      img.classList.add("previous");
-    }
-  });
-  if (document.querySelector(".previous") != null) {
-    //select and show next image
-    if (document.querySelector(".previous").previousSibling != null) {
-      const nextImg = document.querySelector(".previous").previousSibling;
-      document.querySelector(".previous").classList.remove("previous");
-      nextImg.classList.add("visible");
-      nextImg.classList.remove("hidden");
-      nextImg.classList.add("previous");
-    } else {
-      //img will rotate back to last img when reaching first image
-      document.querySelector(".previous").classList.remove("previous");
-      const lastImg = document.querySelector(".last-img");
-      lastImg.classList.add("visible");
-      lastImg.classList.remove("hidden");
-      lastImg.classList.add("previous");
-    }
+
+function handleCarouselClick(direction) {
+  const currentImg = document.querySelector(".visible");
+  const firstImg = document.querySelector(".first-img");
+  const lastImg = document.querySelector(".last-img");
+
+  if (currentImg) {
+    currentImg.classList.toggle("visible");
+    currentImg.classList.toggle("hidden");
+
+    const nextImg =
+      direction === "right"
+        ? currentImg.nextElementSibling || firstImg
+        : currentImg.previousElementSibling || lastImg;
+
+    nextImg.classList.toggle("visible");
+    nextImg.classList.toggle("hidden");
   }
-});
+}
+
+rightBtn.addEventListener("click", () => handleCarouselClick("right"));
+leftBtn.addEventListener("click", () => handleCarouselClick("left"));
