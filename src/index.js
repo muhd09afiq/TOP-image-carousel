06af02873817cad8bdedae5e802e42340810cb6c
blazion09 <muhd09afiq@gmail.com>
2024-05-10 11:28:34 +0800

    feat: add navigation dots

--- src/index.js
+++ src/index.js
@@ -11,7 +11,9 @@ const frame = document.querySelector(".img-frame");
 
 const imgArray = [img1, img2, img3, img4, img5];
 
+//create img and circle
 imgArray.forEach((img, index) => {
+  //create img
   const photo = new Image();
   photo.src = img;
   frame.appendChild(photo);
@@ -24,12 +26,24 @@ imgArray.forEach((img, index) => {
     photo.classList.add("visible");
   }
   if (index === imgArray.length - 1) photo.classList.add("last-img");
+
+  //create circle
+  const circle = document.createElement("div");
+  circle.classList.add("navigation-dot");
+  const container = document.querySelector(".circle-container");
+  container.appendChild(circle);
+  if (index === 0) {
+    circle.classList.add("active");
+    circle.classList.add("first-circle");
+  }
+  if (index === imgArray.length - 1) circle.classList.add("last-circle");
 });
 
 const rightBtn = document.querySelector(".right-btn");
 const leftBtn = document.querySelector(".left-btn");
 
 function handleCarouselClick(direction) {
+  //change active image
   const currentImg = document.querySelector(".visible");
   const firstImg = document.querySelector(".first-img");
   const lastImg = document.querySelector(".last-img");
@@ -46,7 +60,24 @@ function handleCarouselClick(direction) {
     nextImg.classList.toggle("visible");
     nextImg.classList.toggle("hidden");
   }
+  //change active circle
+  const currentCircle = document.querySelector(".active");
+  const firstCircle = document.querySelector(".first-circle");
+  const lastCircle = document.querySelector(".last-circle");
+
+  if (currentCircle) {
+    currentCircle.classList.toggle("active");
+
+    const nextCircle =
+      direction === "right"
+        ? currentCircle.nextElementSibling || firstCircle
+        : currentCircle.previousElementSibling || lastCircle;
+
+    nextCircle.classList.toggle("active");
+  }
 }
 
-rightBtn.addEventListener("click", () => handleCarouselClick("right"));
-leftBtn.addEventListener("click", () => handleCarouselClick("left"));
+window.addEventListener("DOMContentLoaded", () => {
+  rightBtn.addEventListener("click", () => handleCarouselClick("right"));
+  leftBtn.addEventListener("click", () => handleCarouselClick("left"));
+});
